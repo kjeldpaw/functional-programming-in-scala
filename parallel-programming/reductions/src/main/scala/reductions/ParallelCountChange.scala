457879ebf7d55b8ccf88bf4870375a0d03580a1e
pfk <paw.figge.kjeldgaard@systematic.com>
2016-09-23 14:32:43 +0200

    Start on parellel count change

--- parallel-programming/reductions/src/main/scala/reductions/ParallelCountChange.scala
+++ parallel-programming/reductions/src/main/scala/reductions/ParallelCountChange.scala
@@ -14,7 +14,7 @@ object ParallelCountChangeRunner {
     Key.exec.maxWarmupRuns -> 40,
     Key.exec.benchRuns -> 80,
     Key.verbose -> true
-  ) withWarmer(new Warmer.Default)
+  ) withWarmer (new Warmer.Default)
 
   def main(args: Array[String]): Unit = {
     val amount = 250
@@ -43,28 +43,46 @@ object ParallelCountChangeRunner {
 object ParallelCountChange {
 
   /** Returns the number of ways change can be made from the specified list of
-   *  coins for the specified amount of money.
-   */
+    * coins for the specified amount of money.
+    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    ???
+    def countChangeIter(money: Int, coins: List[Int]): Int = {
+      if (money < 0) {
+        0
+      } else if (money == 0) {
+        1
+      } else if (coins.isEmpty) {
+        0
+      } else {
+        countChangeIter(money - coins.head, coins) + countChangeIter(money, coins.tail)
+      }
+    }
+
+    if (money == 0) {
+      0
+    } else if (coins.isEmpty) {
+      0
+    } else {
+      countChangeIter(money, coins)
+    }
   }
 
   type Threshold = (Int, List[Int]) => Boolean
 
   /** In parallel, counts the number of ways change can be made from the
-   *  specified list of coins for the specified amount of money.
-   */
+    * specified list of coins for the specified amount of money.
+    */
   def parCountChange(money: Int, coins: List[Int], threshold: Threshold): Int = {
     ???
   }
 
   /** Threshold heuristic based on the starting money. */
   def moneyThreshold(startingMoney: Int): Threshold =
-    ???
+  ???
 
   /** Threshold heuristic based on the total number of initial coins. */
   def totalCoinsThreshold(totalCoins: Int): Threshold =
-    ???
+  ???
 
 
   /** Threshold heuristic based on the starting money and the initial list of coins. */
